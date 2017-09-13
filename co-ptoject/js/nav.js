b7d8adb12617d54b3d798ac01b985ade66afa631
wqj19980925 <1714796246@qq.com>
2017-09-13 17:14:23 +0800

    fd

--- co-ptoject/js/nav.js
+++ co-ptoject/js/nav.js
@@ -1,30 +1,30 @@
 (function() {
-  var oCat = document.querySelector('#cat');
-  myajax.get('http://h6.duchengjiu.top/shop/api_cat.php', {}, function(error, responseText) {
-    var json = JSON.parse(responseText); //返回的整个json对象
-    var data = json.data; //json对象当中的data是一个数组
-    for (var i = 0; i < data.length; i++) {
-      var obj = data[i]; //数组里面的每一项是一个商品分类的对象
-      oCat.innerHTML += `<li><a href="list.html?cat_id=${obj.cat_id}">${obj.cat_name}</a></li>`;
-    }
-  });
-  var topNav = document.querySelector('.nav-list');
-  var topDis = getAllTop(topNav);
-  window.onscroll = function(e) {
-    var nowTop = document.documentElement.scrollTop || document.body.scrollTop;
-    if (nowTop >= topDis) {
-      topNav.style.position = 'fixed';
-      topNav.style.top = 0;
-    } else {
-      topNav.style.position = 'relative';
-    }
-  };
+    var oCat = document.querySelector('#cat');
+    myajax.get('http://h6.duchengjiu.top/shop/api_cat.php', {}, function(error, responseText) {
+        var json = JSON.parse(responseText); //返回的整个json对象
+        var data = json.data; //json对象当中的data是一个数组
+        for (var i = 0; i < data.length; i++) {
+            var obj = data[i]; //数组里面的每一项是一个商品分类的对象
+            oCat.innerHTML += `<li><a href="list.html?cat_id=${obj.cat_id}">${obj.cat_name}</a></li>`;
+        }
+    });
+    var topNav = document.querySelector('.nav-list');
+    var topDis = getAllTop(topNav);
+    window.addEventListener(scroll, function(e) {
+        var nowTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if (nowTop >= topDis) {
+            topNav.style.position = 'fixed';
+            topNav.style.top = 0;
+        } else {
+            topNav.style.position = 'relative';
+        }
+    });
 
-  function getAllTop(obj) {
-    var allTop = obj.offsetTop;
-    while (obj = obj.offsetParent) {
-      allTop += obj.offsetTop;
+    function getAllTop(obj) {
+        var allTop = obj.offsetTop;
+        while (obj = obj.offsetParent) {
+            allTop += obj.offsetTop;
+        }
+        return allTop;
     }
-    return allTop;
-  }
 })();
