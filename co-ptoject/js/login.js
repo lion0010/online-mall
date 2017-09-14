9635181012841f341feee892b630c1b1cc214b0d
wqj19980925 <1714796246@qq.com>
2017-09-14 12:06:01 +0800

    hg

--- co-ptoject/js/login.js
+++ co-ptoject/js/login.js
@@ -1,33 +1,42 @@
 var oUsername = document.querySelector('input[name=username]');
 var oPassword = document.querySelector('input[name=password]');
 var oBtn = document.querySelector('input[type=button]');
-oBtn.onclick = function() {
-  //js校验
-  //直接调用登录的接口
-  myajax.post('http://h6.duchengjiu.top/shop/api_user.php', {
-    status: 'login',
-    username: oUsername.value,
-    password: oPassword.value
-  }, function(error, responseText) {
-    var json = JSON.parse(responseText);
-    console.log(json);
-    localStorage.token = json.data.token;
-    localStorage.username = json.data.username;
-    console.log(localStorage.token);
-    console.log(localStorage.username);
-    // alert(json.message)
-    if (json.message == "登录成功") {
-      location.href = "../index.html";
+oBtn.addEventListener("click", function() {
+    //js校验
+    //直接调用登录的接口
+    myajax.post('http://h6.duchengjiu.top/shop/api_user.php', {
+        status: 'login',
+        username: oUsername.value,
+        password: oPassword.value
+    }, function(error, responseText) {
+        var json = JSON.parse(responseText);
+        console.log(json);
+        localStorage.token = json.data.token;
+        localStorage.username = json.data.username;
+        console.log(localStorage.token);
+        console.log(localStorage.username);
+        // alert(json.message)
+        if (json.message == "登录成功") {
+            location.href = "../index.html";
+        }
+
+        if (json.message === "少传参数username") {
+            toast("请输入用户名")
+        }
+        if (json.message === "密码最小长度为6位") {
+            toast("请输入密码，且密码最小长度为6位")
+        }
+        if (json.message === "密码错误") {
+            toast("登录失败，请检查用户名或密码")
+        }
+        if (json.message === "用户名不存在") {
+            toast("用户名不存在，请前往注册")
+
+        }
+    });
+})
+window.onkeyup = function(event) {
+    if (event.keyCode === 13) {
+        oBtn.click();
     }
-    
-    if(json.message === "少传参数username"){
-      toast("请输入用户名")
-    }
-    if(json.message === "密码最小长度为6位"){
-      toast("请输入密码，且密码最小长度为6位")
-    }
-    if(json.message === "密码错误"){
-      toast("登录失败，请检查用户名或密码")
-    }
-  });
 }
